0ee8096f6f872f9292ccdc9e01bc985421ed5de1
Brian Shaler <brian@shaler.name>
2012-10-19 01:49:25 -0700

    dump stopwords into db

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -3,11 +3,13 @@
  **/
 
 var mongoose = require('mongoose'),
+  fs = require('fs'),
 	conf = require('node-config'),
 	crypto = require('crypto');
 
 // Models
-var User = mongoose.model('User');
+var User = mongoose.model('User'),
+    JunkTopic = mongoose.model('JunkTopic');
 
 exports.controller = function(req, res, next) {
 	Controller.call(this, req, res, next);
@@ -19,4 +21,59 @@ exports.controller = function(req, res, next) {
 		self.render("dashboard/dashboard");
 	}
 	// end /dashboard/index
+	
+	self.junk_topics = function () {
+	  
+	  check_junk_topics();
+	  
+	  function check_junk_topics () {
+			JunkTopic.count({}, function (err, count) {
+				if (err) throw err;
+				
+				if (count == 0) {
+					fs.readFile('cache/junk_topics.json', 'utf8', add_missing_junk_topics);
+				} else {
+					finished();
+				}
+			});
+		}
+		
+		function add_missing_junk_topics (err, content) {
+			var junk_topics = [];
+			if (!err) {
+				junk_topics = JSON.parse(content);
+			}
+			
+			if (junk_topics.length > 0) {
+				JunkTopic.find({"text": {"$in":junk_topics}}, function (err, existing) {
+					if (err) throw err;
+		
+					var new_junk_topics = [];
+		
+					junk_topics.forEach(function (t) {
+						var found = false;
+						existing.forEach(function (et) {
+							if (!found && et.text.toLowerCase() == t.toLowerCase()) {
+								found = true;
+							}
+						});
+						if (!found) {
+							var jt = new JunkTopic({text: t});
+							jt.save(function (err) {
+								if (err) throw err;
+							});
+						}
+					});
+					finished();
+				});
+			} else {
+				finished();
+			}
+			
+		}
+		
+		function finished () {
+		  self.send("Done.");
+	  }
+	}
 };
